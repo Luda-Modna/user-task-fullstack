f58baecc3e171b60f4a36d1756fa15046bf750ef
Viktoriia <pecourses>
2024-01-21 16:18:55 +0200

    Add load users with image

--- client/src/components/UsersList/index.jsx
+++ client/src/components/UsersList/index.jsx
@@ -2,15 +2,34 @@ import { connect } from 'react-redux';
 import BeatLoader from 'react-spinners/BeatLoader';
 import styles from './UsersList.module.sass';
 import defImage from './defaultPhoto.jpg';
+import { useEffect, useState } from 'react';
 
-export const UsersList = ({ users, isFetching, error }) => {
+export const UsersList = ({ isFetching, error }) => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/users')
+      .then(response => response.json())
+      .then(data => setUsers(data.data))
+      .catch(err => console.log('err :>> ', err));
+  }, []);
+
   return (
     <>
       <BeatLoader loading={isFetching} />
       {error && <div>!!!ERROR!!!</div>}
       <ul>
         {users.map(u => (
-          <li key={u.id}>{JSON.stringify(u)}</li>
+          <li key={u.id}>
+            <img
+              className={styles.userImage}
+              src={
+                u.image ? `http://localhost:5000/images/${u.image}` : defImage
+              }
+              alt={`${u.firstName} ${u.lastName}`}
+            />
+            <p>{JSON.stringify(u)}</p>
+          </li>
         ))}
       </ul>
     </>
