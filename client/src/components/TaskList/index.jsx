71e22edbab79c00db5bfb97d708d203d9dc94f69
Luda Modna <ludmilamodna3@gmail.com>
2025-10-25 20:52:17 +0300

    Add task list + styles

--- client/src/components/TaskList/index.jsx
+++ client/src/components/TaskList/index.jsx
@@ -1,5 +1,65 @@
-export const TaskList = () => {
-    return <div>Tasks Page</div>;
+import { connect } from 'react-redux';
+import BeatLoader from 'react-spinners/BeatLoader';
+import {
+  deleteUserTaskThunk,
+  getUserTasksThunk,
+} from '../../store/slices/tasksSlice';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import styles from './TaskList.module.sass';
+
+export const TaskList = ({
+  tasks,
+  isFetching,
+  error,
+  getUserTasks,
+  deleteTask,
+}) => {
+  const { userId } = useParams();
+
+  useEffect(() => {
+    getUserTasks(userId);
+  }, []);
+
+  const handleDelete = taskId => {
+    deleteTask({ userId, taskId });
+  };
+
+  return (
+    <>
+      <BeatLoader loading={isFetching} />
+      {error && <div>ERROR</div>}
+      {tasks.length === 0 && !isFetching ? (
+        <p className={styles.noTaskText}> No tasks yet</p>
+      ) : (
+        <ul className={styles.taskList}>
+          {tasks.map(t => (
+            <li className={styles.taskListItems} key={t.id}>
+              <p className={styles.taskText}>{t.body}</p>
+              <p className={styles.taskText}>{t.deadline}</p>
+              <button
+                className={styles.deleteTaskBtn}
+                onClick={() => {
+                  handleDelete(t.id);
+                }}
+              >
+                {' '}
+                -{' '}
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 };
 
-export default TaskList;
+const mapStateToProps = ({ tasksData }) => tasksData;
+
+const mapDispatchToProps = dispatch => ({
+  getUserTasks: id => dispatch(getUserTasksThunk(id)),
+  deleteTask: ({ userId, taskId }) =>
+    dispatch(deleteUserTaskThunk({ userId, taskId })),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(TaskList);
